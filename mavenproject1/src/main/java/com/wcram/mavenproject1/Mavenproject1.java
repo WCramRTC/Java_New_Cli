3b024106acb7cf8579f637dfc6ce593314f3d405
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2023-10-17 12:33:31 -0700

    Add Java Project

---
+++ mavenproject1/src/main/java/com/wcram/mavenproject1/Mavenproject1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.wcram.mavenproject1;
+
+/**
+ *
+ * @author WCram
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
